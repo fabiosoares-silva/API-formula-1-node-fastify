8d28c4dff2218153c10269832be6de76a609c3d2
fabiosoares-silva <fabiosores.soares@gmail.com>
2024-11-12 15:30:32 -0300

    Incluir camada repositories com arquivo para tratamento de dados

---
+++ src/repositories/data-repository.ts
@@ -0,0 +1,35 @@
+export const teams = [
+    {
+        id: 1,
+        name: 'McLaren',
+        base: 'Woking, United Kingdom'
+    },
+    {
+        id: 2,
+        name: 'Mercedes',
+        base: 'Brackley, United Kingdom'
+    },
+    {
+        id: 3,
+        name: 'Red Bull Racing',
+        base: 'Minton Keynes, United Kingdom'
+    }
+];
+
+export const drivers = [
+    {
+        id: 1,
+        name: 'Max Vertappen',
+        team: 'Red Bull Racing'
+    },
+    {
+        id: 2,
+        name: 'Lewis Hamilton',
+        team: 'Ferrari'
+    },
+    {
+        id: 3,
+        name: 'Lando Norris',
+        team: 'McLaren'
+    }
+];
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,12 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import {teams, drivers} from './repositories/data-repository';
 
 const server = fastify({logger: true});
 
 server.register(cors, {
     origin: '*'
 });
-
-const teams = [
-    {
-        id: 1,
-        name: 'McLaren',
-        base: 'Woking, United Kingdom'
-    },
-    {
-        id: 2,
-        name: 'Mercedes',
-        base: 'Brackley, United Kingdom'
-    },
-    {
-        id: 3,
-        name: 'Red Bull Racing',
-        base: 'Minton Keynes, United Kingdom'
-    }
-];
-
-const drivers = [
-    {
-        id: 1,
-        name: 'Max Vertappen',
-        team: 'Red Bull Racing'
-    },
-    {
-        id: 2,
-        name: 'Lewis Hamilton',
-        team: 'Ferrari'
-    },
-    {
-        id: 3,
-        name: 'Lando Norris',
-        team: 'McLaren'
-    }
-];
 
 server.get<{Params: DriverParams}>('/drivers/:id', async (request, response) => {
     const id = parseInt(request.params.id);
